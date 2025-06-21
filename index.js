2278e4d67c5916b7a6de409fef3d07d2f75e7843
yousefrawag <yousefrawag908@gmail.com>
2025-06-21 06:46:12 +0300

    new-one

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.use((req, res, next) => {
   next();
 });
 const corsOptions = {
-  origin: ["https://rayapal-crm.netlify.app" ,  "http://localhost:5173"], // specify the origin that you want to allow
+  origin: ["https://rayapal-crm.netlify.app" ,  "http://localhost:3002"], // specify the origin that you want to allow
   methods: 'GET,POST,PUT,DELETE , PATCH ', // specify the methods you want to allow
   allowedHeaders: 'Content-Type,Authorization', // specify the headers you want to allow
   credentials: true // Allow credentials to be included in the request
